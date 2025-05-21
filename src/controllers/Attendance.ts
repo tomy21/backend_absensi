0b322ca6ad2aa8dc3e13f55f29ca00566dba735b
tomy21 <tomy21.agung@gmail.com>
2025-05-21 16:22:04 +0700

    upload

--- src/controllers/Attendance.ts
+++ src/controllers/Attendance.ts
@@ -300,8 +300,8 @@ export const getAllAttendance = async (
   res: Response
 ): Promise<void> => {
   try {
-    const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 10;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     const totalItems = await dbMain.detailUsers.count();
@@ -324,7 +324,7 @@ export const getAllAttendance = async (
       skip,
       take: limit,
       orderBy: {
-        Date: "desc",
+        CreatedAt: "desc",
       },
     });
 
